2c99f4415a847e225bd8f332b704b3773b7c0f05
unknown <cchisomfrancis@gmail.com>
2022-06-01 16:36:09 +0100

    blogapi

--- Apipro/settings.py
+++ Apipro/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+    'rest_framework', 'firstapi',
 ]
 
 MIDDLEWARE = [
